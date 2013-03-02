9ba5d2ecebc7ed6f00d486221b763f8c335a589a
Richard Schneider <richard@dhcp184-49-112-107.hil-rduhshx.dca.wayport.net>
2013-03-02 00:10:44 -0500

    Engine read-eval-print loop in place

---
+++ src/board.c
@@ -0,0 +1,5 @@
+#include <stdio.h>;
+
+void sayhi(){
+    printf("hi!\n");
+}
---
+++ src/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	char *input = NULL;
+	size_t inputLen = 0;
+	ssize_t inputRead;
+	
+	char *command;
+	char *args;
+	
+	int i;
+	
+	//Engine read-eval-print loop
+	for(;;){
+		//Get input, skip if any errors, and trim newline off the end
+		inputRead = getline(&input, &inputLen, stdin);
+		if(inputRead <= 1){
+			continue;
+		}
+		input[inputRead - 1] = '\0';
+		
+		//Convert input to lower case
+		for(i = 0; input[i]; i++){
+			input[i] = tolower(input[i]);
+		}
+		
+		//Split at the first space to separate command and arguments
+		//NOTE: This changes (invalidates?) the input variable
+		command = strtok(input, " ");
+		args = strtok(NULL, "");
+		
+		//stop - stops the engine
+		if(strcmp("stop", command) == 0){
+			break;
+		}
+		
+		if(strcmp("sayhi", command) == 0){
+			sayhi();
+		}
+		
+		//Invalid command
+		else{
+			printf("Invalid command: %s\n", command);
+		}
+	}
+	
+	free(input);
+	return 0;
+}
