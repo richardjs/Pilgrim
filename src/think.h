ca28b4c9b0331ee70c80405960c6337841007a4f
Richard Schneider <richard@schneiderbox.net>
2013-04-25 22:03:32 -0500

    Tweaks to algorithm parameters

--- src/think.h
+++ src/think.h
@@ -5,10 +5,10 @@
 #include "board.h"
 
 //Total number of iterations; should control overall playing strength
-#define ITERATIONS 50000 
+#define ITERATIONS 100000 
 
 //Parameter controlling exploration vs. exploitation in tree search
-#define UCTK .4 
+#define UCTK .4
 
 //Number of simulations a node must have before it's added to the tree 
 #define MIN_SIMS 5 
